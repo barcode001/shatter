f29cd9b1694abc38c9c33c41804756e1669fb80f
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-05-23 16:17:20 -0500

    Build for GitHub Pages base /shatter/

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/shatter/',            // your repo name
-  build: {
-    outDir: 'docs'
-  },
+
   plugins: [react()],
   server: {
     historyApiFallback: true, // <- for React Router
