fc0eb91c76a9f9cd14f8b2257a41417044788bc9
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-05-23 18:58:11 -0500

    fix bugs html

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   // in dev (“serve”) use root paths; in build, prefix with /shatter/
+//     base: command === "serve" ? "/" : "/shatter/",
+//   plugins: [react()],
+//   server: {
+//     historyApiFallback: true, // <- for React Router
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/shatter/',            // your repo name
-
-  plugins: [react()],
-  server: {
-    historyApiFallback: true, // <- for React Router
+export default defineConfig(({ command, mode }) => {
+  return {
+    // Dev server: use root (‘/’)
+    // Production build: prefix asset URLs with '/shatter/'
+    base: command === 'serve' ? '/' : '/shatter/',
+    plugins: [react()],
+    server: {
+      // Ensures client-side routing works in dev
+      historyApiFallback: true,
+    },
   }
 })
