23a5bc6d8f6a285f5c40aacd11eede46aae3d15a
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-05-23 13:57:03 -0500

    chore: publish production build 6

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/shatter/',   // <-- your GitHub repo name
   plugins: [react()],
   server: {
     historyApiFallback: true, // <- for React Router
