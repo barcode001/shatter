50a3834779c567df5604d387976d022190f75f4a
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-05-24 18:04:29 -0500

    Deploy Shatter site

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,4 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   // in dev (“serve”) use root paths; in build, prefix with /shatter/
-//     base: command === "serve" ? "/" : "/shatter/",
-//   plugins: [react()],
-//   server: {
-//     historyApiFallback: true, // <- for React Router
-//   }
-// })
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig(({ command }) => ({
-//   // base: command === 'build' ? '/' : '/shatter/', // ✅ only set /shatter/ for production
-//    base: '/shatter/', // 👈 IMPORTANT
-//   plugins: [react()],
-//   build: {
-//     outDir: 'docs',
-//   },
-// }));
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
