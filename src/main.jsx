b901572f03de6c0c7dbe6dc8d5419c43284d43a7
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-05-24 17:56:55 -0500

    html update mian.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./styles/main.scss";
-// import ContactPage from "./ContactPage";
+import "./styles/main.scss"; // or .css
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/shatter">
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/shatter">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
