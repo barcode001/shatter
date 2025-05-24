62e8a47366ed0539459da3b70263d5955caf8cdf
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-05-24 18:34:24 -0500

    Deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./styles/main.scss"; // or .css
+// import "./styles/main.scss"; // or .css
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/shatter">
+    <BrowserRouter basename="/shatter/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
-  // base:'/shatter/',
-  base: '/shatter/',
+export default defineConfig({
+  base: '/shatter/', // <-- match your GitHub repo name
   plugins: [react()],
   build: {
     outDir: 'docs',
     emptyOutDir: true,
   },
-}))
+})
