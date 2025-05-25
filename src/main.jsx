fa80da199996eecdddda9f06b3bdaaf587b53e13
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-05-25 00:20:41 -0500

    Fix Vite inject and deploy 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
 import "./styles/main.scss";
+import { BrowserRouter } from "react-router-dom";
+
+const isProd = import.meta.env.MODE === "production";
+const basename = isProd ? "/shatter" : "/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/shatter/">
+    <BrowserRouter basename="/shatter">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
