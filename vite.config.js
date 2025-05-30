880ed44c5f468bbd38be56107d6542aed7f9aebd
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-05-30 07:00:15 -0500

    Fix Vite config and clean index.html

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '', // leave this empty for Netlify custom domain
   plugins: [react()],
   build: {
     outDir: 'dist',
