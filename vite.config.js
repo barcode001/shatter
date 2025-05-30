6fe7b714f41b0abc23ccab4ba6eaff504d21a197
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-05-30 07:11:31 -0500

    Rebuild for Netlify

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '', // leave this empty for Netlify custom domain
+  base: '/shatter/',
   plugins: [react()],
   build: {
     outDir: 'dist',
